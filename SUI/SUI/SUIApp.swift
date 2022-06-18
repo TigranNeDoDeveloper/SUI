5ff02e8fab9c60f01dd67b43c2b1c39aee421026
Тигран Левонян <levonyan.t@bk.ru>
2022-06-18 20:06:35 +0300

    Initial Commit

---
+++ SUI/SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by Тигран Левонян on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
