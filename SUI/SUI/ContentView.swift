1ecd7023f4379c073a4522647b6cd7db923c6548
Тигран Левонян <levonyan.t@bk.ru>
2023-01-26 23:45:34 +0300

    Second lesson update,added 2 buttons for transaction and refilling balance

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isOnToggle = false
+    @State var isBalanceShown = false
+    @State var isBalanceChanged = false
+    @State var isTransactionChanged = false
+    @State var balanceValue = 0
+    @State var informationAboutBalance = ""
 
 
     var body: some View {
@@ -16,15 +21,51 @@ struct ContentView: View {
             ZStack {
                 HStack {
                     VStack {
-                        Text("Кошелёк")
+                        Button {
+                            isBalanceShown = true
+                        } label: {
+                            Text("Пополнить")
+                        }
+                        .actionSheet(isPresented: $isBalanceShown, content: {
+                            ActionSheet(title: Text("Пополнить баланс"),
+                                        message: Text("Вы хотите загрузить фото?"),
+                                        buttons: [.destructive(Text("Пополнить баланс"),
+                                        action: {
+                                isBalanceChanged = true
+                                informationAboutBalance = "Баланс пополнен успешно"
+                                balanceValue += 500
+                            }), .cancel()])
+                        })
+
+                        Spacer().frame(height: 30)
+                        Button {
+                            isTransactionChanged = true
+                        } label: {
+                            Text("Открыть счёт")
+                        }
+                        .actionSheet(isPresented: $isTransactionChanged, content: {
+                            ActionSheet(title: Text("Информация о переводе"),
+                                        message: Text("Вы уверены что хотите выполнить перевод?"),
+                                        buttons: [.destructive(Text("Перевести средства"),
+                                        action: {
+                                isBalanceChanged = true
+                                informationAboutBalance = "Перевод выполнен успешно"
+                                balanceValue -= 250
+                            }), .cancel()])
+                        })
+                        Spacer().frame(height: 30)
                         Text("Профиль")
-                        Spacer().frame(height: 500)
+                        Spacer().frame(height: 30)
                     }
                     Spacer()
                 }
                 Spacer()
                 RoundedRectangle(cornerRadius: 10).fill(Color.yellow).offset(x: isOnToggle ? 100 : 0)
-                Text("Текущий баланс 1500 рублей").offset(x: isOnToggle ? 100 : 0)
+                VStack {
+                    Text(informationAboutBalance).offset(x: isOnToggle ? 100 : 0)
+                    Spacer().frame(height: 30)
+                    Text("Текущий баланс \(balanceValue) рублей").offset(x: isOnToggle ? 100 : 0)
+                }
             }
             Toggle(isOn: $isOnToggle, label: {
                 Text("Показать настройки")
