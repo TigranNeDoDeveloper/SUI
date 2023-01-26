0b6860d531553712a63c5085cd75a31b198fbc2d
Тигран Левонян <levonyan.t@bk.ru>
2023-01-26 16:54:15 +0300

    First lesson done,added 2 alerts

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var isFirstAlertShown = false
+    @State var isSecondAlertShown = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        showAlert
+    }
+
+    private var showAlert: some View {
+        VStack {
+            Button {
+                isFirstAlertShown = true
+            } label: {
+                Text("Пример Alert")
+            }
+            .alert(isPresented: $isFirstAlertShown) {
+                Alert(title: Text("Загрузка"),
+                      message: Text("Вы уверены?"),
+                      primaryButton: .destructive(Text("Да"), action: {
+                    print("test")
+                }),
+                      secondaryButton: .cancel())
+            }
+            Divider()
+            Button {
+                isSecondAlertShown = true
+            } label: {
+                Text("Загрузить данные")
+            }
+            .actionSheet(isPresented: $isSecondAlertShown, content: {
+                ActionSheet(title: Text("Загрузка"),
+                            message: Text("Вы хотите загрузить фото?"),
+                            buttons: [.destructive(Text("Загрузить"),
+                            action: {
+                            print("Пошла загрузка")
+                }), .cancel()])
+            })
+        }
     }
 }
 
