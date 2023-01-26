86e12089e21b9a65f3e426f9be4fccdee0e36c19
Тигран Левонян <levonyan.t@bk.ru>
2023-01-26 22:34:23 +0300

    Second lesson done,added 2 view with balance

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOnToggle = false
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                HStack {
+                    VStack {
+                        Text("Кошелёк")
+                        Text("Профиль")
+                        Spacer().frame(height: 500)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                RoundedRectangle(cornerRadius: 10).fill(Color.yellow).offset(x: isOnToggle ? 100 : 0)
+                Text("Текущий баланс 1500 рублей").offset(x: isOnToggle ? 100 : 0)
+            }
+            Toggle(isOn: $isOnToggle, label: {
+                Text("Показать настройки")
+            }).padding()
+        }.animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3), value: isOnToggle)
     }
 }
 
